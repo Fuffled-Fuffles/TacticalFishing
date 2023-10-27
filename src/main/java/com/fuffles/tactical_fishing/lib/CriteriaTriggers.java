8341d75acd605c910a31bc3883aeead66bd2fa1b
Fuffled-Fuffles <68608998+Fuffled-Fuffles@users.noreply.github.com>
2023-10-27 15:04:10 +0200

    swap to fuffles.tactical_fishing package format

--- src/main/java/com/fuffles/tactical_fishing/lib/CriteriaTriggers.java
+++
@@ -1,8 +0,0 @@
-package com.fuffles.tactical_fishing.lib;
-
-import com.fuffles.tactical_fishing.common.advancements.criterion.FishingCraftingTrigger;
-
-public class CriteriaTriggers 
-{
-	public static final FishingCraftingTrigger FISHING_CRAFTING = net.minecraft.advancements.CriteriaTriggers.register(new FishingCraftingTrigger());
-}
